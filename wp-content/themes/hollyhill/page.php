36004e26e883bc3235c058e8883f7effc1e39205
john <john@johnharrisdesign.com>
2014-12-07 23:50:04 -0500

    Updated nav on child campus

--- wp-content/themes/hollyhill/page.php
+++ wp-content/themes/hollyhill/page.php
@@ -1,11 +1,8 @@
 <?php
 
-//var_dump(get_the_id());
-var_dump(get_child_campus_id());
 get_campus_color_scheme(get_the_id());
 
 if(is_ancestor(get_child_campus_id()) || is_page(get_child_campus_id())) {
-    var_dump('is_child');
     remove_action('genesis_after_header', 'genesis_do_nav');
     add_action('genesis_after_header', 'hh_child_campus_do_nav');
 }
