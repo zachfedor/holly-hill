f2c74de92675778ef0179252b009947b23b36b82
john <john@johnharrisdesign.com>
2014-12-09 11:47:33 -0500

    Updated sidebar nav

--- wp-content/themes/hollyhill/lib/layout/sidebar.php
+++ wp-content/themes/hollyhill/lib/layout/sidebar.php
@@ -30,76 +30,77 @@ function child_do_sidebar() {
 	if ( !function_exists( 'dynamic_sidebar' ) || !dynamic_sidebar( 'Alternative Sidebar' ) ) {
 }}
 
-add_action('genesis_before_sidebar_widget_area', 'hh_page_children_nav');
-function hh_page_children_nav()
-{
-    // If this is a page AND it isn't search
-    if (is_page() && !is_search() ) {
+add_action('genesis_before_sidebar_widget_area', 'hh_sidebar_menu_subnav');
+function hh_sidebar_menu_subnav(){
 
-            // Get the global post object
-            global $post;
+    // Get the global post object
+    global $post;
 
-            // Get the post ID from the post object
-            $page_id = $post->ID;
+    // Get the post ID from the post object
+    $page_id = $post->ID;
 
-            // Get the post's ancestors (ie. parents and grandparents)
-            $page_ancestors = get_post_ancestors($page_id);
+    $menu = 'Adult Campus Navigation';
 
-            // Get the post's children
-            $children = get_pages('child_of='.$page_id);
+    //var_dump(get_campus_color_scheme($page_id));
+    if(get_campus_color_scheme($page_id) == 'child-campus'){
+        $menu = 'Child Campus Navigation';
+    }
 
+    $args = array(
+        'order'                  => 'ASC',
+        'orderby'                => 'menu_order',
+        'post_type'              => 'nav_menu_item',
+        'post_status'            => 'publish',
+        'output'                 => ARRAY_A,
+        'output_key'             => 'menu_order',
+        'nopaging'               => true,
+        'update_post_term_cache' => false
+    );
+    $items = wp_get_nav_menu_items( $menu, $args );
 
+    // Get the post's ancestors (ie. parents and grandparents)
+    $page_ancestors = get_post_ancestors($page_id);
 
-            if (count($page_ancestors) > 0) {
-                $page_ancestor = reset($page_ancestors);
-            } else {
-                $page_ancestor = $page_id;
+    if (count($page_ancestors) > 0) {
+        $page_ancestor = reset($page_ancestors);
+    } else {
+        $page_ancestor = $page_id;
+    }
+
+    if(get_campus_color_scheme($page_id) == 'child-campus') {
+
+    }
+
+    // Get the page's children pages
+    $children = get_pages('child_of='.$page_ancestor);
+
+    $menu_root_id = null;
+
+    foreach($items as $item) {
+
+        if($item->object_id == $page_ancestor){
+            $menu_root_id = $item->ID;
+        }
+
+        if($page_ancestor == get_child_campus_id()){
+            //var_dump($item->ID);
+            $page_ancestor = $page_id;
+            $menu_root_id = $item->ID;
+        }
+    }
+
+    if(count($children) > 0) {
+        echo '<section class="page-navigation widget">';
+        echo '<h3>' . get_the_title($page_ancestor) . '</h3>';
+        echo '<ul>';
+        foreach($items as $item){
+            if($item->menu_item_parent == $menu_root_id){
+                echo '<li><a href="'. $item->url .'">'. $item->title .'</a></li>';
             }
-
-            //var_dump(get_child_campus_id());
-            //var_dump($page_ancestors);
-            //var_dump($page_ancestor);
-            //var_dump(count($children));
-
-            // If the ancestor is child_campus homepage
-            if($page_ancestor == get_child_campus_id()){
-                $page_ancestor = $page_id;
-            }
-
-
-            if($page_ancestor == $page_id && count($children) > 0) {
-                echo '<section class="page-navigation widget">';
-                echo '<h3>' . get_the_title($page_ancestor) . '</h3>';
-
-                $args = array(
-                    'authors' => '',
-                    'child_of' => $page_ancestor,
-                    'date_format' => get_option('date_format'),
-                    'depth' => 1,
-                    'echo' => 1,
-                    'exclude' => '',
-                    'include' => '',
-                    'link_after' => '',
-                    'link_before' => '',
-                    'post_type' => 'page',
-                    'post_status' => 'publish',
-                    'show_date' => '',
-                    'sort_column' => 'menu_order, post_title',
-                    'sort_order' => '',
-                    'title_li' => '',
-                    'walker' => ''
-                );
-
-                echo '<ul>';
-                wp_list_pages($args);
-                echo '</ul>';
-                echo '</section>';
-            }
-            /**/
-
-
-            ?>
-    <?php
+        }
+        echo '</ul>';
+        echo '</section>';
     }
 
 }
+
